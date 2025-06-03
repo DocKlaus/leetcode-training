4fd3f4a445a86cce4d60a96a345e362555667427
DocKlaus <doct.klaus@gmail.com>
2025-06-03 10:12:18 +0300

    22 task

---
+++ tasks/22 (комбо скобок).py
@@ -0,0 +1,16 @@
+def generateParenthesis(n: int) -> list[str]:
+    result = []
+
+    def recurs(current: str, open: int, close: int):
+        if len(current) == 2 * n:
+            result.append(current)
+            return
+        if open < n:
+            recurs(current + '(', open + 1, close)
+        if close < open:
+            recurs(current + ')', open, close + 1)
+
+    recurs('', 0, 0)
+    return result
+
+print(generateParenthesis(3))
